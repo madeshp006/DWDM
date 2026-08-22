14de412826fc3eadf95a8c2d67bc907528856ea7
madeshp006 <154950509+madeshp006@users.noreply.github.com>
2026-08-22 11:13:00 +0530

    Delete EXPRIMENTS directory

--- EXPRIMENTS/D2/5th.R
+++
@@ -1,7 +0,0 @@
-x <- AirPassengers
-
-hist(x,
-     breaks = seq(100, 700, by = 150),
-     xlim = c(100, 700),
-     main = "AirPassengers Histogram",
-     xlab = "Passengers")
--- EXPRIMENTS/D2/8th.R
+++
@@ -1,5 +0,0 @@
-boxplot(mpg ~ cyl,
-        data = mtcars,
-        xlab = "Cylinders",
-        ylab = "Miles per Gallon",
-        main = "MPG vs Cylinders")
